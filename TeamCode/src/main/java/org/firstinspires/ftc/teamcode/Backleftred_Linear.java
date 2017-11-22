ee1621ff2e216a099944000ea2373e91ad3b6ba3
GreenMachine808 <mherrera23@punahou.edu>
2017-11-22 11:57:20 -1000

    Changed the name and Backleftred_Linear
    Changed the name for a program and created a program called Backleftred_Linear.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Backleftred_Linear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Backleftred_Linear.java
@@ -34,9 +34,9 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@Autonomous(name="Backrightblue_Linear", group="Pushbot")
+@Autonomous(name="Backleftred_Linear", group="Pushbot")
 //@Disabled
-public class Backrightblue_Linear extends LinearOpMode {
+public class Backleftred_Linear extends LinearOpMode {
 
 
     private ElapsedTime     runtime = new ElapsedTime();
@@ -88,9 +88,9 @@ public class Backrightblue_Linear extends LinearOpMode {
         // Step through each leg of the path,
         // Note: Reverse movement is obtained by setting a negative distance (not speed)
 
-        encoderDrive(0.5,   37, 37, 9.0);
-        encoderDrive(0.5, -12, 12, 4.0);
-        encoderDrive(0.5, 22, 22, 7.0);
+        encoderDrive(0.5,   2, 2, 4.0);
+        encoderDrive(0.5, 12, -12, 5.0);
+        encoderDrive(0.5, 37, 37, 9.0);
         arm(1.0, 3000);
         encoderDrive(0.5, -1.5, -1.5, 3.0);
 
