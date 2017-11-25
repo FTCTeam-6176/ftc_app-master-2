cc91d91878431167fc576532b3e85f1be78e9b4c
Wokikawa <wokikawa23@punahou.edu>
2017-11-24 23:21:21 -1000

    fixing program
    fixing program

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Backleftblue_Linear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Backleftblue_Linear.java
@@ -34,7 +34,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@Autonomous(name="Backleftblue_Linear", group="Pushbot")
+@Autonomous(name="Backleftblue_Linear3", group="Pushbot")
 //@Disabled
 public class Backleftblue_Linear extends LinearOpMode {
 
