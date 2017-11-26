9ed9c05d93971d470c9fc52fdeb73e7d5bac09e4
mokikawa <mark.okikawa@gmail.com>
2017-11-26 02:28:26 -1000

    Working Autonomous Code
    Reads jewel color and turn accordingly
    if (colory.red() > colory.blue(). Need to stop looping.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CoachMark_BasicOpMode_Linear_NoEncoders.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CoachMark_BasicOpMode_Linear_NoEncoders.java
@@ -36,69 +36,40 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DistanceSensor;
-import com.qualcomm.robotcore.util.ElapsedTime;
-
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
-
-import java.util.Locale;
-
-// **Added imports
-
-/**
- * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
- * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
- * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
- * class is instantiated on the Robot Controller and executed.
- *
- * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
- * It includes all the skeletal structure that all linear OpModes contain.
- *
- * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
- */
+// import com.qualcomm.robotcore.util.ElapsedTime;
 
 @Autonomous(name="Color Sensor Test", group="No Encoder")
 //@Disabled  //Comment this out to add to the opmode list
 public class CoachMark_BasicOpMode_Linear_NoEncoders extends LinearOpMode {
 
-    // Use Pushbot's Hardware
-    Hardwaremap robot = new Hardwaremap();
-
     // Declare OpMode members.
-    private ElapsedTime runtime = new ElapsedTime();
-    private DcMotor left_Drive = null;
-    private DcMotor right_Drive = null;
-
-    //** Added declarations
-    private ColorSensor colory;
-    private DistanceSensor sensorDistance;
-    private CRServo Sensor_arm = null;
+    // private ElapsedTime runtime = new ElapsedTime();
 
     @Override
-    public void runOpMode() throws InterruptedException {
+    public void runOpMode() {
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
         // Initialize the hardware variables. Note that the strings used here as parameters
         // to 'get' must correspond to the names assigned during the robot configuration
         // step (using the FTC Robot Controller app on the phone).
-        left_Drive  = hardwareMap.get(DcMotor.class, "left_Drive");
-        right_Drive = hardwareMap.get(DcMotor.class, "right_Drive");
-        Sensor_arm  = hardwareMap.get(CRServo.class, "Sensor_arm");
+        DcMotor left_Drive = hardwareMap.get(DcMotor.class, "left_Drive");
+        DcMotor right_Drive = hardwareMap.get(DcMotor.class, "right_Drive");
+        CRServo Sensor_arm = hardwareMap.get(CRServo.class, "Sensor_arm");
 
-        // Initialize drive train system variables
-        // the init() method of the hardware class does all the work here
-        robot.init(hardwareMap);
+        // Most robots need the motor on one side to be reversed to drive forward
+        // Reverse the motor that runs backwards when connected directly to the battery
+        left_Drive.setDirection(DcMotor.Direction.FORWARD);
+        right_Drive.setDirection(DcMotor.Direction.REVERSE);
 
         // get a reference to the color sensor
-        colory = hardwareMap.get(ColorSensor.class, "colory");
+        ColorSensor colory = hardwareMap.get(ColorSensor.class, "colory");
 
         // hsvValues is an array that will hold the hue, saturation, and value information
         float hsvValues[] = {0F, 0F, 0F};
 
         // values is a reference to the hsvValues array
-        // final float values[] = hsvValues;
+        final float values[] = hsvValues;
 
         // sometimes it helps to multiply the raw RGB values with a scale factor
         // to amplify/attenuate the measured values.
@@ -106,19 +77,17 @@ public class CoachMark_BasicOpMode_Linear_NoEncoders extends LinearOpMode {
 
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
-        runtime.reset();
 
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
 
             // Extend turn on LED Jewel Arm
-            colory.enableLed(true);  // Turn on LED
             Sensor_arm.setPower(1.0);
-            sleep(10000);
+            sleep(1500);
             Sensor_arm.setPower(0);
-            colory.enableLed(false);
+            sleep(5000);
             Sensor_arm.setPower(-1.0);
-            sleep(9000);
+            sleep(1500);
             Sensor_arm.setPower(0);
 
             // convert the RGB values to HSV values
@@ -130,13 +99,31 @@ public class CoachMark_BasicOpMode_Linear_NoEncoders extends LinearOpMode {
                     hsvValues);
 
             // send the info back to driver station using telemetry function
-            telemetry.addData("Distance (cm)", String.format(Locale.US, "%.02f", sensorDistance.getDistance(DistanceUnit.CM)));
             telemetry.addData("Alpha", colory.alpha());
             telemetry.addData("Red", colory.red());
             telemetry.addData("Green", colory.green());
             telemetry.addData("blue", colory.blue());
             telemetry.addData("Hue", hsvValues[0]);
+            telemetry.update();
 
+            if (colory.red() > colory.blue())
+            {
+                sleep(5000);
+                left_Drive.setPower(-.25);
+                right_Drive.setPower(.25);
+                sleep(1000);
+                left_Drive.setPower(0);
+                right_Drive.setPower(0);
+            }
+            else
+            {
+                sleep(5000);
+                left_Drive.setPower(.25);
+                right_Drive.setPower(-.25);
+                sleep(1000);
+                left_Drive.setPower(0);
+                right_Drive.setPower(0);
+            }
         }
     }
 }
