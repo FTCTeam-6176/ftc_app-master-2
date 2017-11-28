9920d14af4e0bb37525438a77ddff7d5b732727a
Wokikawa <wokikawa23@punahou.edu>
2017-11-28 07:55:22 -1000

    Continuting
    Continuting commands to gliph

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousColorSensorByWyattblueleft.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousColorSensorByWyattblueleft.java
@@ -117,6 +117,12 @@ public class AutonomousColorSensorByWyattblueleft extends LinearOpMode {
                 Sensor_arm.setPower(-1.0);
                 sleep(6000);
                 Sensor_arm.setPower(0);
+                left_Drive.setPower(.25);
+                right_Drive.setPower(-.25);
+                sleep(1000);
+                left_Drive.setPower(1);
+                right_Drive.setPower(1);
+                sleep(500);
             }
             else
             {
